a7aa2120834aed52a26a38b45472b254602fef85
Sobithanm <120468421+Sobithanm@users.noreply.github.com>
2023-04-19 11:58:43 +0530

    Prop drilling

---
+++ Props_Drilling/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Parent from './Props_Drilling/Parent';
+
+const App = () => {
+  return (
+    <>
+    <Parent data={100}/>
+    </>
+  )
+}
+
+export default App;
---
+++ Props_Drilling/Child.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Child = (props) => {
+  return (
+    <h1>The drilled data is : {props.data}</h1>
+  )
+}
+
+export default Child
